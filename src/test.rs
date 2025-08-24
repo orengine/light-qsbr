5b52db7c3c8f6640750cecf86fdbfe007f852c7b
Eugene Usachev <usachblog@gmail.com>
2025-08-25 00:03:30 +0300

    Make tests more predictable on weak-memory models

--- src/test.rs
+++ src/test.rs
@@ -28,7 +28,7 @@ fn wait_new_epoch() {
     let start_epoch = local_manager().shared_manager().current_epoch();
 
     while start_epoch == local_manager().shared_manager().current_epoch() {
-        local_manager().maybe_pass_epoch(OrengineInstant::now());
+        local_manager().maybe_pass_epoch(OrengineInstant::from(std::time::Instant::now()));
 
         thread::sleep(Duration::from_micros(1));
     }
